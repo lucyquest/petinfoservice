2cfebda7aafd0621ad8c78ec3bc6dd64a6f4bc7b
Clayton Townsend II <clayton@claytontii.com>
2023-10-11 17:30:20 -0500

    add getting VCS (git) commit from buildinfo

--- main.go
+++ main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+	"runtime/debug"
+)
+
+func getVCSCommit(b *debug.BuildInfo) (string, bool) {
+	for _, s := range b.Settings {
+		if s.Key == "vcs.revision" {
+			return s.Value, true
+		}
+	}
+	return "", false
+}
+
 func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		slog.Info("unable to read build info")
+	}
+
+	commit, ok := getVCSCommit(info)
+	if !ok {
+		slog.Info("commit information not found in binary")
+	}
+
+	slog.Info("Starting petinfoservice", "commit", commit)
 }
