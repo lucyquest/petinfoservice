2e8a9731f423f710bfc077af4469c9f71400aee9
Clayton Townsend II <clayton@claytontii.com>
2023-10-11 17:46:56 -0500

    add slog logging using a context derived from a interrupt from the OS, and starting the server that is closed on the context being completed

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
+	"os/signal"
 	"runtime/debug"
+	"syscall"
+
+	"github.com/lucyquest/petinfoservice/service"
 )
 
 func getVCSCommit(b *debug.BuildInfo) (string, bool) {
@@ -16,17 +21,37 @@ func getVCSCommit(b *debug.BuildInfo) (string, bool) {
 }
 
 func main() {
+	// Setup a context that is cancelled when we get an interupt
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+	go func() {
+		osSignal := make(chan os.Signal, 1)
+		signal.Notify(osSignal, syscall.SIGTERM, os.Interrupt)
+
+		<-osSignal
+		cancelFunc()
+	}()
+
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
 
 	info, ok := debug.ReadBuildInfo()
 	if !ok {
-		slog.Info("unable to read build info")
+		slog.InfoContext(ctx, "unable to read build info")
 	}
 
 	commit, ok := getVCSCommit(info)
 	if !ok {
-		slog.Info("commit information not found in binary")
+		slog.InfoContext(ctx, "commit information not found in binary")
 	}
 
-	slog.Info("Starting petinfoservice", "commit", commit)
+	slog.InfoContext(ctx, "Starting petinfoservice", "commit", commit)
+
+	serv := service.Service{}
+	if err := serv.Open(); err != nil {
+		slog.ErrorContext(ctx, "could not start petinfoservice", "error", err)
+		return
+	}
+
+	<-ctx.Done()
+	serv.Close()
 }
