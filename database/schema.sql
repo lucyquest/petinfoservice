878281c70aad73d739b6aa2595a7428eb62d1a7d
Clayton Townsend II <clayton@claytontii.com>
2023-11-03 13:23:59 -0500

    create idempotency sql table

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,14 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TABLE idempotency (
+  id UUID NOT NULL,
+  function TEXT NOT NULL,
+  step TEXT NOT NULL,
+  last_used TIMESTAMP NOT NULL
+);
+
 CREATE TABLE pets (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   name TEXT NOT NULL,
-  date_of_birth TIMESTAMP NOT NULL
-)
+  date_of_birth DATE NOT NULL
+);
